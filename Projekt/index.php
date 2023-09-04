a2604a0409c19f39ed08d38c8b770ff1be9d4b2c
geki7 <83777643+geki7@users.noreply.github.com>
2023-09-04 21:14:23 +0200

    PHP registracija

--- Projekt/index.php
+++ Projekt/index.php
@@ -8,7 +8,8 @@ const korisnik = "root";
 const lozinka = "s6K8yzPb";
 const baza = "webdip2022x007";
 
-if(isset($_GET['prijavi'])){
+//prijava
+if(isset($_GET['login-button'])){
     
     $greska_krivi_unos="";
 
@@ -30,7 +31,6 @@ if(isset($_GET['prijavi'])){
   
 
     if(mysqli_num_rows($rezultat)>0){
-        
         while($red=mysqli_fetch_assoc($rezultat)){
             $korisnicko_ime=$red['Korisnicko_ime'];
             $sifra=$red['Lozinka'];
@@ -47,6 +47,95 @@ if(isset($_GET['prijavi'])){
         Sesija::kreirajKorisnika($korisnicko_ime,$uloga);
         echo $_SESSION['korisnik'];
         echo $_SESSION['uloga'];
+    }
+}
+//registracija 
+if (isset($_POST['registriraj'])) {
+
+    $greska_polje="";
+    $poruka = "";
+    $email_greska = "";
+    $lozinka_greska = "";
+    $potvrda_lozinka_greska="";
+    $email_postoji_greska="";
+
+    $ime = $_POST['ime'];
+    $prezime = $_POST['prezime'];
+    $email = $_POST['email'];
+    $korime = $_POST['korisnicko_ime'];
+    $lozinka = $_POST['lozinka'];
+    $potvrda_lozinka = $_POST['potvrda_lozinke'];
+
+    foreach ($_POST as $key => $value) {
+        if (empty($value)) {
+            $poruka .= "Nije upisano " . $key . "<br>";
+            $greska_polje.="Nije prazno";
+        } elseif ($key == "email") {
+
+            $upit="SELECT * FROM `Korisnik` WHERE Email='{$email}'";
+
+            $rezultat=$baza->selectDB($upit);
+
+            if(mysqli_num_rows($rezultat)>0){
+                $email_postoji_greska="Email već postoji";
+                $greska_polje.="Nije prazno";
+            }
+            $regex = "/^[A-Za-z0-9._]{1,64}@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.(info|hr|com)$/";
+
+            if (!preg_match($regex, $email)) {
+                $email_greska = "Neispravan format emaila";
+                $greska_polje.="Nije prazno";
+            }
+        } elseif ($key == "lozinka") {
+
+            function pregled_lozinke($lozinka)
+            {
+                $mala_slova = false;
+                $velika_slova = false;
+                $brojevi = false;
+                $razmak=true;
+                $duljina_lozinke=false;
+               
+                if(strlen($lozinka)>=15 && strlen($lozinka)<=25){
+                    $duljina_lozinke=true;
+                }
+                for ($i = 0; $i < strlen($lozinka); $i++) {
+                    $znak = $lozinka[$i];
+                    if (ctype_lower($znak)) {
+                        $mala_slova = true;
+                    } elseif (ctype_upper($znak)) {
+                        $velika_slova = true;
+                    } elseif (ctype_digit($znak)) {
+                        $brojevi = true;
+                    }
+                    elseif(strpos($lozinka,' ')==true){
+                        $razmak=false;
+                    }   
+                }
+                return $mala_slova && $velika_slova && $brojevi && $razmak && $duljina_lozinke;
+            }
+            if (!pregled_lozinke($lozinka)) {
+                $lozinka_greska = "Lozinka nije u dobrom formatu";
+                $greska_polje.="Nije prazno";
+            }
+        }
+        elseif($key=="potvrda_lozinke"){
+            if($lozinka!=$potvrda_lozinka){
+                $potvrda_lozinka_greska="Lozinke nisu iste";
+                $greska_polje.="Nije prazno";
+            }
+        }
+    }
+
+    if(empty($greska_polje)){
+        $sol="sha256kript";
+        $kriptirano=$lozinka.$sol;
+        $nova_lozinka=sha1($kriptirano);
+
+        $upit="INSERT INTO `korisnik`(`Korisnik_ID`, `Ime`, `Prezime`, `Slika_Korisnika`, `Email`, `Korisnicko_ime`, 
+        `Lozinka`, `Potvrda_lozinkeSH`, `Uloga`) VALUES ('','{$ime_prezime}','{$datum_rodenja}','{$email}',
+        '{$korime}','{$lozinka}','{$nova_lozinka}',3)";
+        $rezultat=$baza->updateDB($upit);
     }
 }
 ?>
@@ -98,12 +187,34 @@ if(isset($_GET['prijavi'])){
                 <input type="text" id="username" name="username" required>
                 <label for="password">Šifra:</label>
                 <input type="password" id="password" name="password" required>
-                <button type="submit" id="prijavi" class="login-button">Prijavi se</button>
+                <button type="submit" class="login-button">Prijavi se</button>
             </form>
         </div>
     </div>
 
     <div id="registrationForm" class="popup-form">
+    <?php
+        if (isset($poruka)) {
+            echo "$poruka";
+            echo "<br>";
+        }
+        if (isset($email_greska)) {
+            echo "$email_greska";
+            echo "<br>";
+        }
+        if (isset($lozinka_greska)) {
+            echo "$lozinka_greska";
+        }
+        echo "<br>";
+        if (isset($potvrda_lozinka_greska)) {
+            echo "$potvrda_lozinka_greska";
+        }
+        echo "<br>";
+        if (isset($email_postoji_greska)) {
+            echo "$email_postoji_greska";
+        }
+        echo "<br>";
+    ?>
         <div class="form-container">
             <span class="close" onclick="closeRegistrationForm()">&times;</span>
             <h2>Registracija</h2>
